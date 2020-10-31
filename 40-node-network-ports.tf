9658d6f62c7cd9098392b04a2a97719d192e2f9d
Klaus Frank <git@frank.fyi>
2020-10-31 22:34:29 +0100

    Update worker node config

--- 40-node-network-ports.tf
+++ 40-node-network-ports.tf
@@ -1,15 +1,15 @@
 resource "openstack_networking_port_v2" "k8s-worker-port" {
-  count          = "${var.worker-node-count}"
+  count          = var.worker-node-count
   name           = "k8s-worker-port_${count.index}"
-  network_id     = "${openstack_networking_network_v2.network_k8s.id}"
+  network_id     = openstack_networking_network_v2.network_k8s.id
   admin_state_up = true
   no_fixed_ip    = true
 }
 
 resource "openstack_networking_port_v2" "k8s-master-port" {
-  count          = "${var.master-node-count}"
+  count          = var.master-node-count
   name           = "k8s-master-port_${count.index}"
-  network_id     = "${openstack_networking_network_v2.network_k8s.id}"
+  network_id     = openstack_networking_network_v2.network_k8s.id
   admin_state_up = true
   no_fixed_ip    = true
 }
---
+++ 45-worker-nodes.tf
@@ -0,0 +1,14 @@
+resource "openstack_compute_instance_v2" "k8s-worker" {
+  count = var.worker-node-count
+  name = "k8s-worker-${count.index}"
+  image_name = var.image-name
+  flavor_name = var.worker-node-image-flavor
+  key_pair = var.key-pair
+  config_drive = true
+  security_groups = [
+    openstack_networking_secgroup_v2.secgroup_k8s-cluster.name
+  ]
+  network {
+    port = openstack_networking_port_v2.k8s-worker-port.*.id[count.index]
+  }
+}
