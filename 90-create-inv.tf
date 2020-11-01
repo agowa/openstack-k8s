a1f48ef67f1c1a84cd4853989d65e7117ba50610
Klaus Frank <git@frank.fyi>
2020-11-01 21:39:58 +0100

    create inv using local_file

--- 90-create-inv.tf
+++ 90-create-inv.tf
@@ -1,43 +1,10 @@
-resource "null_resource" "ansible-inventory" {
-
-  depends_on = [
-    openstack_compute_instance_v2.k8s-master,
-    openstack_compute_instance_v2.k8s-worker,
-    openstack_networking_port_v2.k8s-master-port,
-    openstack_networking_port_v2.k8s-worker-port,
-    openstack_networking_subnet_v2.subnet_k8s
-  ]
-
-  # Clear inventory
-  provisioner "local-exec" {
-    command = "echo \"\" > inventory.ini"
-  }
-
-  ##Create Masters Inventory
-  provisioner "local-exec" {
-    command =  "echo \"[kube-master]\" >> inventory.ini"
-  }
-  provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.k8s-master.*.name, openstack_compute_instance_v2.k8s-master.*.network.0.fixed_ip_v6))}\" >> inventory.ini"
-  }
-
-  ##Create ETCD Inventory
-  provisioner "local-exec" {
-    command =  "echo \"\n[etcd]\" >> inventory.ini"
-  }
-  provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.k8s-master.*.name, openstack_compute_instance_v2.k8s-master.*.network.0.fixed_ip_v6))}\" >> inventory.ini"
-  }
-
-  ##Create Nodes Inventory
-  provisioner "local-exec" {
-    command =  "echo \"\n[kube-node]\" >> inventory.ini"
-  }
-  provisioner "local-exec" {
-    command =  "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.k8s-worker.*.name, openstack_compute_instance_v2.k8s-worker.*.network.0.fixed_ip_v6))}\" >> inventory.ini"
-  }
-
-  provisioner "local-exec" {
-    command =  "echo \"\n[k8s-cluster:children]\nkube-node\nkube-master\" >> inventory.ini"
-  }
+resource "local_file" "AnsibleInventory" {
+  content = templatefile("90-inventory.tmpl", {
+      kube_master = openstack_compute_instance_v2.k8s-master
+      kube_etcd   = openstack_compute_instance_v2.k8s-master
+      kube_node   = openstack_compute_instance_v2.k8s-worker
+    }
+  )
+  filename = "inventory.ini"
+  file_permission = "0440"
 }
